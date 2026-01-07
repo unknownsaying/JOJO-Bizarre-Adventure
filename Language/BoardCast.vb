088609b7fcdab27f21fe84e7282b138e04acb740
unknownsaying <15368104568@189.cn>
2026-01-07 16:12:45 +0800

    Update BoardCast.vb

--- Language/BoardCast.vb
+++ Language/BoardCast.vb
@@ -1,2 +1,4 @@
 TryCast(https://bandainamco-am.co.jp/am/vg/jojols/)
 DirectCast(https://jojowiki.com/)
+TryCast(https://ja.wikipedia.org/wiki/%E3%82%B9%E3%82%BF%E3%83%B3%E3%83%89_(%E3%82%B8%E3%83%A7%E3%82%B8%E3%83%A7%E3%81%AE%E5%A5%87%E5%A6%99%E3%81%AA%E5%86%92%E9%99%BA)
+DirectCast(https://zh.wikipedia.org/wiki/%E6%9B%BF%E8%BA%AB_(JoJo%E7%9A%84%E5%A5%87%E5%A6%99%E5%86%92%E9%9A%AA)
